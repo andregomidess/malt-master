05618865c6dc09839953a40c80b31aafd319554e
andregomidess <andre.gomides@tech4h.com.br>
2025-09-23 22:44:32 -0300

    feat: adds jwt guards in routes

--- src/catalog/controllers/equipment.controller.ts
+++ src/catalog/controllers/equipment.controller.ts
@@ -6,50 +6,60 @@ import {
   Param,
   Delete,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { EquipmentService } from '../services/equipment.service';
 import { type EquipmentInputUnion } from '../inputs/equipment.input';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('equipments')
 export class EquipmentController {
   constructor(private readonly equipmentService: EquipmentService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll() {
     return await this.equipmentService.findAll();
   }
 
   @Put()
+  @UseGuards(JwtAuthGuard)
   async saveEquipment(@Body() equipmentInput: EquipmentInputUnion) {
     return await this.equipmentService.save(equipmentInput);
   }
 
   @Get('kettles')
+  @UseGuards(JwtAuthGuard)
   async findKettles() {
     return await this.equipmentService.findKettles();
   }
 
   @Get('fermenters')
+  @UseGuards(JwtAuthGuard)
   async findFermenters() {
     return await this.equipmentService.findFermenters();
   }
 
   @Get('chillers')
+  @UseGuards(JwtAuthGuard)
   async findChillers() {
     return await this.equipmentService.findChillers();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async findById(@Param('id') id: string) {
     return await this.equipmentService.findById(id);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async deleteById(@Param('id') id: string) {
     return await this.equipmentService.softDelete(id);
   }
 
   @Post(':id/recovery')
+  @UseGuards(JwtAuthGuard)
   async recoveryEquipment(@Param('id') id: string) {
     return await this.equipmentService.recovery(id);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,35 +6,42 @@ import {
   Param,
   Put,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { type UserInput } from './inputs/user.input';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Put()
+  @UseGuards(JwtAuthGuard)
   upsertUser(@Body() userInput: UserInput) {
     return this.usersService.updateUser(userInput);
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   getUserById(@Param('id') id: string) {
     return this.usersService.getUser(id);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   getUsers() {
     return this.usersService.getUsers();
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   deleteUserById(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
   }
 
   @Post(':id/recovery')
+  @UseGuards(JwtAuthGuard)
   restoreUser(@Param('id') id: string) {
     return this.usersService.recoveryUser(id);
   }
